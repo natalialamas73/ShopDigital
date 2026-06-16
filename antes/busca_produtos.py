d9cb51caab17ef2a3fe5e4f9aeb93c0079c7cc1d
natalialamas73 <87660580+natalialamas73@users.noreply.github.com>
2026-06-16 17:25:07 -0300

    Create busca_produtos.py

---
+++ antes/busca_produtos.py
@@ -0,0 +1,7 @@
+# antes/busca_produtos.py
+# Arquivo secundário para simular a interface de busca
+def exibir_busca():
+    print("Bem-vindo à ShopDigital!")
+    termo = input("Digite o que deseja buscar: ")
+    # Aqui o sistema chamaria a função do db_connection.py
+    return termo
